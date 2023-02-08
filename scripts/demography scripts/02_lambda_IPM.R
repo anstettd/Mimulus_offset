f73ef94f9a3b0a924ee92e4d1ac05e8b8f2c97a6
Amy Angert <amyangert@Amys-Mac-Pro.local>
2023-02-08 09:25:41 -0800

    full parameters data frame (still a few issues to resolve)

--- scripts/demography scripts/02_lambda_IPM.R
+++ scripts/demography scripts/02_lambda_IPM.R
@@ -13,13 +13,6 @@ rm(list = ls(all=TRUE))
 
 # Make vector of packages needed
 packages_needed <- c("lme4", "glmmTMB", "tidyverse")
-require(lme4)
-require(glmmTMB)
-require(plyr)
-require(dplyr)
-require(ggplot2)
-require(tidyr)
-require(gridExtra)
 
 # Install packages needed (if not already installed)
 for (i in 1:length(packages_needed)){
@@ -120,53 +113,46 @@ fruit=c()
   ### 1E. Size distribution of recruits ###
   #*******************************************************************************
   
-  # Calculate mean size in year t+1 of plants that were new that year (unmeasured at time t)
-  recruit.size.mean <- tapply(data$logSizeNext[is.na(data$logSize)], data$SiteYear[is.na(data$logSize)], FUN="mean") %>% data.frame()
+  # Calculate mean  and standard deviation of size in year t+1 of plants that were new that year (unrecorded at time t)
+  recruit_size <- data %>% 
+    filter(is.na(logSize)) %>% 
+    group_by(SiteYear) %>% 
+    summarise(recruit.size.mean = mean(logSizeNext),
+              recruit.size.sd = sd(logSizeNext))
   
-  # Calculate st dev of size in year t+1 of plants that were new that year (unmeasured at time t)
-  recruit.size.sd <- tapply(data$logSizeNext[is.na(data$logSize)], data$SiteYear[is.na(data$logSize)], FUN="sd") %>% data.frame()
-  
-  # Assemble mean and sd into same data frame
-  recruit_size <- data.frame(rownames(recruit.size.mean), recruit.size.mean, recruit.size.sd)
-  recruit_size <- rename(recruit_size, SiteYear="rownames.recruit.size.mean.", recruit.logSize.mean=".",recruit.logSize.sd="..1")
-  
-  # Assign 0 values to sites with no recruitment (mean & SD = NA) or only 1 recruit (mean = value but SD= A)
-  recruit_size$recruit.logSize.mean[is.na(recruit_size$recruit.logSize.mean)]=0 
-  recruit_size$recruit.logSize.sd[is.na(recruit_size$recruit.logSize.sd)]=0 
-  # TO DO: Address Seema's comment about assigning 0s: "WARNING! MAY NEED TO MODIFY THIS!"
-
-  #*******************************************************************************
+   #*******************************************************************************
   ### 1F. Create data frame of site-specific parameter estimates and join all estimates ###
   #*******************************************************************************
   
   params <- data.frame(params)
-  params <- full_join(params,growth) %>% full_join(fruit) %>% full_join(recruit_size)
+  params <- full_join(params,growth) %>% full_join(fruit) %>% full_join(recruit_size) %>% 
+    replace_na(list(recruit.size.mean=0, recruit.size.sd=0)) # assign 0s to sites with no recruitment (mean & SD = NA) or only 1 recruit (mean = value but SD=NA)
+  # TO DO: decide if there any other situations where param values of NA should be re-coded as 0.
+    
   
   #*******************************************************************************
   ### 1G. Number of seeds per fruit ###
   #*******************************************************************************
   
-  # Note: these are site-specific, but not site-by-year-specific, values
+  # Note: these are site-specific, but not site-by-year-specific, values, but we want to copy the site-specific values into each site-year level
   
   # obtain mean seed counts per fruit per site
+  seeds.per.siteyear <- data %>% 
+    group_by(SiteYear, Site) %>% 
+    summarize(SeedCtSY = mean(SeedCt, na.rm=T)) # this yields NaN for some site-year combos
+
   seeds.per.site <- data %>% 
     group_by(Site) %>% 
-    summarize(SeedCt = mean(SeedCt, na.rm=T)) 
+    summarize(seed.ct = mean(SeedCt, na.rm=T)) 
   
-  #seeds.per.site = tapply(data$SeedCt, data$SiteYear, FUN=mean, na.rm=T) 
+  seeds.per.site <- left_join(seeds.per.siteyear, seeds.per.site) %>% select(SiteYear, seed.ct)
   
-  # make into a data frame
   seeds.per.site <- data.frame(seeds.per.site, rownames(seeds.per.site)) 
-  
-  # define column names for merging
-  colnames(seeds.per.site)=c("seed.ct","SiteYear") 
-  
-  # replace missing values with NA
-  seeds.per.site$seed.ct[seeds.per.site$seed.ct=="Inf"]=NA
   
   # join seeds per fruit to other parameters
   params <- left_join(params, seeds.per.site)
                       
+  
   #*******************************************************************************
   ### 1H. Establishment probability ###
   #*******************************************************************************
@@ -175,7 +161,7 @@ fruit=c()
   recruit.number <- data %>% 
     dplyr::filter(is.na(logSize)) %>% 
     group_by(SiteYear) %>% 
-    summarise(recruit.number = n())
+    summarise(recruit.number = n()) 
     
   # Obtain total fruit count per site at year = t
   fruits.per.site <- site_fruit_count_data %>% 
@@ -185,21 +171,22 @@ fruit=c()
   
   # Join recruit # and fruits per site into one frame
   # Obtain total seed count per site (= # fruits per site * # seeds per fruit per site) at year = t
-  total.seeds.per.site <- full_join(recruit.number, fruits.per.site) %>% full_join(seeds.per.site)
-    mutate(total.seeds.per.site = )
+  # Obtain establishment probability as # of new recruits at year = t+1/# of seeds at year = t
+  establishment <- full_join(recruit.number, fruits.per.site) %>% full_join(seeds.per.site) %>% 
+    replace_na(list(recruit.number=0, fruits.per.site=0)) %>% # assign 0s to sites with no recruitment or no fertility
+    # NOTE: Amy added this in 2023. Ok?
+    mutate(total.seeds.per.site = fruits.per.site*seed.ct,
+           establishment.prob = ifelse(recruit.number==0, 0, recruit.number/total.seeds.per.site)) 
+    # TO DO: there are a handful of site-years where recruit.number>0 but fruits.per.site=0, resulting in Inf for establishment.prob. Since establishment is non-zero in these site-years, we need a reasonable number for fruits.per.site. Ideas: (a) replace fruits.per.site with mean across other years or (b) replace fruits.per.site with value of 1 because fecundity was very low that year?
   
-  # 
-  total.seeds.per.site=fruits.per.site*seeds.per.site$seed.ct	
-  
-  # Estimate establishment probability as # of new recruits at year = t+1/# of seeds at year = t
-  params$establishment.prob=recruit.number$recruit.number/total.seeds.per.site
-  
-  # Set establishment probability as 0 for Hauser Creek (was calculated as NA because Hauser creek has 0 new recruits)
-  params$establishment.prob[is.na(params$establishment.prob)]=0	
+  # Join with params frame
+    params <- left_join(params, establishment)
 
   # Add separate columns for year and site
-  params=params %>% separate(SiteYear,c("Site","Year"),":",remove=FALSE)
+  params <- params %>% separate(SiteYear, c("Site","Year"), ":", remove=FALSE)
   
+  # Store parameters in .csv file for later use
+  write.csv(params,"data/demography data/vital_rate_coefficients.csv", row.names=FALSE)
   
 #*******************************************************************************
 ### 2. Create site-specific IPMs parameterized by site-specific parameters derived from global vital rates models 
@@ -210,10 +197,8 @@ fruit=c()
   #*******************************************************************************
   
   # remove rows of parameters with NA
-  params=params[complete.cases(params),]
+  params <- params[complete.cases(params), ]
   
-  # Store parameters in .csv file for later use
-  #write.csv(params,"R_output/vital_rate_coefficients.csv",row.names=FALSE)
   
   # remove site x year combinations without parameter estimates
   siteYear=siteYear[siteYear %in% params$SiteYear]
