ebc6ed675230a666ee027af37dfe9f3cfe8f147d
danstett <daniel_anstett@yahoo.ca>
2023-02-15 15:22:52 -0500

    WZA peak windows

--- Genomics_scripts/5.5_peak_win.R
+++ Genomics_scripts/5.5_peak_win.R
@@ -1,5 +1,5 @@
 #############################################################################################################
-## Get peak windwos from manhattan plots with empirical p-values
+## Get peak windows from manhattan plots with empirical p-values
 ## Author Daniel Anstett
 ## 
 ## 
@@ -17,9 +17,9 @@ snps_mat <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_
 snps_map <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_snps_map.csv")
 snps_cmd <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_snps_cmd.csv")
 
-wza_win_mat <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_win_mat.csv")
-wza_win_map <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_win_map.csv")
-wza_win_cmd <- read_csv("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/WZA_win_cmd.csv")
+wza_win_mat <- read_csv("Genomics_scripts/Data/WZA_win_mat.csv")
+wza_win_map <- read_csv("Genomics_scripts/Data/WZA_win_map.csv")
+wza_win_cmd <- read_csv("Genomics_scripts/Data/WZA_win_cmd.csv")
 
 #Filter by Bonferonnii correction alpha critical = 1.29423e-06, aka 5.887988 sigma
 mat_bon <- wza_win_mat %>% filter(approx_p<1.29423e-06)
@@ -27,6 +27,7 @@ map_bon <- wza_win_map %>% filter(approx_p<1.29423e-06)
 cmd_bon <- wza_win_cmd %>% filter(approx_p<1.29423e-06)
 
 #Get peak windows only
+#Filter out windows that are not peak windows
 mat_bon_peak <- mat_bon %>% filter(!win %in% c(9933:9936,9939,9940))
 map_bon_peak <- map_bon %>% filter(!win %in% c(20883:20885,20887))
 cmd_bon_peak <- cmd_bon %>% filter(!win %in% c(9933,9935,9937,9939,9940,20883,20884,20886,20888))
@@ -51,7 +52,7 @@ wza_empri_cmd <- ggplot(data = wza_win_cmd, aes( x = pos/1e6, y = -log10(approx_
   geom_point(aes(color=as.factor(chr), alpha=0.9))+
   geom_line()+
   scale_y_continuous("-log10(WZA Empirical p-value)", limits=c(0,20))+
-  scale_x_continuous("Position (Mbp)",limits=c(397,398))+
+  scale_x_continuous("Position (Mbp)",limits=c(0,410))+ #MODIFY RANGE HERE TO VIEW DATA UP CLOSE
   geom_hline(aes(yintercept = -log10(0.05/dim(wza_win_cmd)[1])), col = "red", lty = 2, lwd = 1)+
   scale_color_manual(values = rep(c("black", "magenta3"), 22 )) +
   theme_classic()+
@@ -77,7 +78,7 @@ wza_empri_map <- ggplot(data = wza_win_map, aes( x = pos/1e6, y = -log10(approx_
   geom_point(aes(color=as.factor(chr), alpha=0.9))+
   geom_line()+
   scale_y_continuous("-log10(WZA Empirical p-value)", limits=c(0,20))+
-  scale_x_continuous("Position (Mbp)",limits=c(397,399))+
+  scale_x_continuous("Position (Mbp)",limits=c(0,410))+ #MODIFY RANGE HERE TO VIEW DATA UP CLOSE
   geom_hline(aes(yintercept = -log10(0.05/dim(wza_win_map)[1])), col = "red", lty = 2, lwd = 1)+
   scale_color_manual(values = rep(c("black", "deepskyblue"), 22 )) +
   theme_classic()+
@@ -103,7 +104,7 @@ wza_empri_mat <- ggplot(data = wza_win_mat, aes( x = pos/1e6, y = -log10(approx_
   geom_point(aes(color=as.factor(chr), alpha=0.9))+
   geom_line()+
   scale_y_continuous("-log10(WZA Empirical p-value)", limits=c(0,20))+
-  scale_x_continuous("Position (Mbp)",limits=c(0,410))+
+  scale_x_continuous("Position (Mbp)",limits=c(0,410))+ #MODIFY RANGE HERE TO VIEW DATA UP CLOSE
   geom_hline(aes(yintercept = -log10(0.05/dim(wza_win_mat)[1])), col = "red", lty = 2, lwd = 1)+
   scale_color_manual(values = rep(c("black", "darkgoldenrod"), 22 )) +
   theme_classic()+
