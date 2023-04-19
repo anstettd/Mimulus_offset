c9995f643a57edc7cbd36e5c63198a98ef15101d
danstett <daniel_anstett@yahoo.ca>
2023-04-19 12:28:32 -0400

    offset_demography edits

--- scripts/Offset_demography/02_offset_plots.R
+++ scripts/Offset_demography/02_offset_plots.R
@@ -14,7 +14,7 @@ library(ggrepel)
 #Import data
 offset_pop <- read_csv("data/genomic_data/offset_pop_9var.csv")
 #offset_pop <- offset_pop %>% filter(Demo_ID!=24)
-offset_pop <- offset_pop %>% filter(Paper_ID!<13)
+offset_pop_10 <- offset_pop %>% filter(Paper_ID<13)
 
 #stats
 lm.1215 <- lm(Lambda_slope~offset_1215,data=offset_pop)
@@ -22,18 +22,23 @@ lm.SSP245 <- lm(Lambda_slope~offset_SSP245,data=offset_pop)
 lm.SSP585 <- lm(Lambda_slope~offset_SSP585,data=offset_pop)
 lm.dist <- lm(Lambda_slope~offset_climate,data=offset_pop)
 
+lm.1215_10 <- lm(Lambda_slope~offset_1215,data=offset_pop_10)
+lm.SSP245_10 <- lm(Lambda_slope~offset_SSP245,data=offset_pop_10)
+lm.SSP585_10 <- lm(Lambda_slope~offset_SSP585,data=offset_pop_10)
+lm.dist_10 <- lm(Lambda_slope~offset_climate,data=offset_pop_10)
 
-#summary(lm.1215)
+
+
 Anova(lm.1215,type="III")
-
-#summary(lm.SSP245)
 Anova(lm.SSP245,type="III")
-
-#summary(lm.SSP585)
 Anova(lm.SSP585,type="III")
-
-#summary(lm.dist)
 Anova(lm.dist,type="III")
+
+
+Anova(lm.1215_10,type="III")
+Anova(lm.SSP245_10,type="III")
+Anova(lm.SSP585_10,type="III")
+Anova(lm.dist_10,type="III")
 
 
 
@@ -145,10 +150,12 @@ ggplot(offset_pop, aes(x=offset_climate, y=Lambda_slope, label=Demo_ID)) +
 
 
 
-###########################
+
+##################################################################################################################
+#Offset pop with 10 initial populations
 
 #2012-2015 offset plotted against lambda only 10
-ggplot(offset_pop, aes(x=offset_1215, y=Lambda_slope, label=Paper_ID)) + 
+ggplot(offset_pop_10, aes(x=offset_1215, y=Lambda_slope, label=Paper_ID)) + 
   geom_point(aes(color=Region), size =4.5)+
   geom_smooth(method=lm,color="black")+
   #  geom_label_repel(aes(label = ID))+
@@ -164,12 +171,12 @@ ggplot(offset_pop, aes(x=offset_1215, y=Lambda_slope, label=Paper_ID)) +
     #legend.position = c(0.85, 0.25),legend.text=element_text(size=14),
     legend.position = c(0.85, 0.85),legend.text=element_text(size=14),
     legend.title=element_text(size=16,face="bold"))
-ggsave("Graphs/1_offset_lambda_1215_only10.pdf",width=8, height = 6, units = "in")
+#ggsave("Graphs/1_offset_lambda_1215_only10.pdf",width=8, height = 6, units = "in")
 
 
 
 #SSP 245 offset plotted against lambda
-ggplot(offset_pop, aes(x=offset_SSP245, y=Lambda_slope, label=Paper_ID)) + 
+ggplot(offset_pop_10, aes(x=offset_SSP245, y=Lambda_slope, label=Paper_ID)) + 
   geom_point(aes(color=Region), size =4.5)+
   geom_smooth(method=lm,color="black")+
   #  geom_label_repel(aes(label = ID))+
@@ -185,10 +192,10 @@ ggplot(offset_pop, aes(x=offset_SSP245, y=Lambda_slope, label=Paper_ID)) +
     legend.position = c(0.85, 0.85),legend.text=element_text(size=14),
     #legend.position = c(0.85, 0.25),legend.text=element_text(size=14),
     legend.title=element_text(size=16,face="bold"))
-ggsave("Graphs/2_offset_lambda_45_only10.pdf",width=7, height = 5, units = "in")
+#ggsave("Graphs/2_offset_lambda_45_only10.pdf",width=7, height = 5, units = "in")
 
 #SSP 585 offset plotted against lambda
-ggplot(offset_pop, aes(x=offset_SSP585, y=Lambda_slope, label=Paper_ID)) + 
+ggplot(offset_pop_10, aes(x=offset_SSP585, y=Lambda_slope, label=Paper_ID)) + 
   geom_point(aes(color=Region), size =4.5)+
   geom_smooth(method=lm,color="black")+
   #  geom_label_repel(aes(label = ID))+
@@ -204,6 +211,24 @@ ggplot(offset_pop, aes(x=offset_SSP585, y=Lambda_slope, label=Paper_ID)) +
     legend.position = c(0.85, 0.85),legend.text=element_text(size=14),
     #legend.position = c(0.85, 0.25),legend.text=element_text(size=14),
     legend.title=element_text(size=16,face="bold"))
-ggsave("Graphs/3_offset_lambda_85_only10.pdf",width=7, height = 5, units = "in")
+#ggsave("Graphs/3_offset_lambda_85_only10.pdf",width=7, height = 5, units = "in")
 
+
+#climate distance plotted against lambda
+ggplot(offset_pop_10, aes(x=offset_climate, y=Lambda_slope, label=Paper_ID)) + 
+  geom_point(aes(color=Region), size =4.5)+
+  geom_smooth(method=lm,color="black")+
+  #  geom_label_repel(aes(label = ID))+
+  geom_text(hjust=-.15, vjust=-.2)+
+  scale_y_continuous(name="Lambda Slope")+
+  scale_x_continuous(name="2012-2015 Climate Distance")+
+  scale_color_manual(values= c("North"="#3399FF", "Center"="#FFCC00", "South"="#FF3333"))+
+  theme_classic() + theme(
+    axis.text.x = element_text(size=14, face="bold"),
+    axis.text.y = element_text(size=14,face="bold"),
+    axis.title.x = element_text(color="black", size=20, vjust = 0.5, face="bold"),
+    axis.title.y = element_text(color="black", size=20,vjust = 2, face="bold",hjust=0.5),
+    legend.position = c(0.85, 0.85),legend.text=element_text(size=14),
+    legend.title=element_text(size=16,face="bold"))
+ggsave("Graphs/4_distance_lambda_only10.pdf",width=8, height = 6, units = "in")
 
