675fb74fffeccd8b16c9ef0136a099bf516b268b
danstett <daniel_anstett@yahoo.ca>
2023-05-07 11:43:34 -0400

    SNP beagle update

--- scripts/Genetic_offset/03_get_climate_SNPs_beagle.R
+++ scripts/Genetic_offset/03_get_climate_SNPs_beagle.R
@@ -16,9 +16,36 @@ climate <- read_csv("data/genomic_data/climate_pop.csv")
 #Import all unique SNPs
 env_snp <- read_csv("data/genomic_data/snp_set_env.csv")
 
-#Import Beagle imputed SNPs
+#Import Beagle imputed snp table for baseline
+pop_order<-read.table("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/beagle/baseline_filtered_variants.QUAL20_MQ40_AN80_MAF0.03_DP1SD.Baypass_table.pop_order", header=F, sep="\t")
+snp<-read.table("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/beagle/baseline_filtered_variants.QUAL20_MQ40_AN80_MAF0.03_DP1SD.Baypass_table", header=F, sep=" ")
+loci<-read.table("/Users/daniel_anstett/Dropbox/AM_Workshop/Large_files/beagle/baseline_filtered_variants.QUAL20_MQ40_AN80_MAF0.03_DP1SD.Baypass_table.loci", header=F, sep="\t")
+colnames(loci) <- c("Chromosome","SNP")
+loci_united <- loci %>% unite(chr_snp,"Chromosome","SNP",sep="_")
+loci_snp <-cbind(loci_united,snp) #add snp lables to rows
 
+#Select BayPass SNPs from merged dataframe
+loci_env <- loci_snp %>% filter (chr_snp %in% as.character(env_snp$chr_snp))
 
+#Generate snpA table for BayPass SNPs
+snp_bay<-data.frame()
+counter<-1
+for (i in seq (2,dim(loci_env)[2]-1,2)){
+  for(j in 1:dim(loci_env)[1]){
+    tmp_total<-as.numeric(loci_env[j,i]) + as.numeric(loci_env[j,i+1])
+    snp_bay[j,counter]<-as.numeric(loci_env[j,i])/tmp_total
+  }
+  counter<-counter+1
+}
+rownames(snp_bay)<- loci_env$chr_snp
+snp_bay_T <- as.data.frame(t(snp_bay))
+snp_clim_bayNA <- cbind(climate,snp_bay_T)
+#write_csv(snp_clim_bayNA, "data/genomic_data/snp_clim_peakbf5NA.csv")
+
+##############################################################################################
+#Remove NA's from all 55 populations
+snp_clim_bay_noNA <- snp_clim_bayNA %>% select_if(~ !any(is.na(.)))
+#write_csv(snp_clim_bay_noNA, "data/genomic_data/snp_clim_peakbf10_noNA.csv")
 
 
 
