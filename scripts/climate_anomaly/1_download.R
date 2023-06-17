32ea69890cb391dbcefa2dab55e2e9e47422d80d
Anstett <anstett3@msu.edu>
2023-06-17 14:17:01 -0400

    downloads update

--- scripts/climate_anomaly/1_download.R
+++ scripts/climate_anomaly/1_download.R
@@ -6,14 +6,15 @@
 ## Last Modified June 17, 2023
 #############################################################################################################
 #Import libraries
-install.packages('/Users/daniel_anstett/Downloads/ClimateNAr.zip', repos=NULL, type='source')
+#install.packages('C:/Users/anstett3/Documents/Genomics/Mimulus_offset/ClimateNAr.zip', repos=NULL, type='source')
 
+library(ClimateNAr)
+library(tidyverse)
 
+#Import
+demo_pop <- read_csv("data/climate_data/demography_pop.csv")
 
-
-
-
-
+demo_clim_1979 <- ClimateNA_API2(ClimateBC_NA='NA', inputFile=demo_pop,period='Year_1979',MSY='M')
 
 
 
